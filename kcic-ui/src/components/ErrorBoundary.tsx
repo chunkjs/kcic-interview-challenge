aa4add944e20d21c319bd8630bf67c9c7e60b1de
valerie <valeriemaite@outlook.com>
2022-07-04 12:30:43 -0400

    🛑 error boundary

---
+++ kcic-ui/src/components/ErrorBoundary.tsx
@@ -0,0 +1,38 @@
+/**
+ * Base class taken from https://react-typescript-cheatsheet.netlify.app/docs/basic/getting-started/error_boundaries/
+ */
+
+import { Component, ErrorInfo, ReactNode } from "react";
+
+interface Props {
+  children?: ReactNode;
+}
+
+interface State {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends Component<Props, State> {
+  public state: State = {
+    hasError: false
+  };
+
+  public static getDerivedStateFromError(_: Error): State {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  public componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    console.error("Uncaught error:", error, errorInfo);
+  }
+
+  public render() {
+    if (this.state.hasError) {
+      return <h1>Unexpected error found</h1>;
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
