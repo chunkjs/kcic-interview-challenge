1aee8c17fdae13afc1462b47d8651b6c80283056
valerie <valeriemaite@outlook.com>
2022-07-04 12:33:52 -0400

    💿 data schema for the add claim component

---
+++ kcic-ui/src/components/modals/schemas.ts
@@ -0,0 +1,44 @@
+const addClaimSchema = {
+    "title": "",
+    "description": "Please enter the fields below.",
+    "type": "object",
+    "required": [
+      "firstName",
+      "lastName"
+    ],
+    "properties": {
+      "firstName": {
+        "type": "string",
+        "title": "First name",
+        "default": "Chuck"
+      },
+      "lastName": {
+        "type": "string",
+        "title": "Last name",
+        "default": "Norris"
+      },
+      "Product": {
+        "type": "string",
+        "title": "Product",
+        "default": "4k TV"
+      },
+      "Issue": {
+        "type": "string",
+        "title": "Issue",
+        "default": "Image of my roundhouse kick broke it"
+      },
+      "PaymentAmount": {
+        "type": "number",
+        "title": "Requested Payment Amount",
+        "default": 1
+      }
+    }
+  };
+
+  const uiSchema = {
+    "Issue": {
+      "ui:widget": "textarea"
+    }
+  };
+
+  export { addClaimSchema, uiSchema }
