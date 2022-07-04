abe9c2dd70cf1e44301a16da53e491f9931855c1
valerie <valeriemaite@outlook.com>
2022-07-04 12:32:24 -0400

    👩🏻‍🔧 claim service

---
+++ kcic-ui/src/services/claim-service.ts
@@ -0,0 +1,55 @@
+const addClaim =(data)=>{
+return fetch('http://localhost:3000/api/Claims', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(data),
+})
+.then(response => response.json())
+.then(data => {
+  return data;
+})
+.catch((error) => {
+  console.error('Error:', error);
+  return error;
+});
+
+};
+const getAllClaims = () =>{
+    return fetch('http://localhost:3000/api/Claims', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      .then(response => response.json())
+      .then(data => {
+        // model the data to comply with ordering requirement, however this operation should live in the db layer
+        const t = _modelClaims(data.claims);
+        console.log(t)
+        _modelClaims(t);
+        return t;
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+        return error;
+      });
+};
+
+const _modelClaims = (claims) =>{
+    const pendingItems = claims.filter((item)=> item.status === 0);
+    const nonPendingItems = claims.filter((item)=> item.status !== 0).sort((claim1, claim2)=>{
+        if(new Date(claim1.dateSubmitted) < new Date(claim2.dateSubmitted)){
+            return -1;
+        }if(new Date(claim1.dateSubmitted) > new Date(claim2.dateSubmitted)){
+            return 1;
+        }
+        return 0;
+    
+    });
+
+    return pendingItems.concat(nonPendingItems);
+}
+
+export { getAllClaims, addClaim }
