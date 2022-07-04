284f9a721b049f0ed8343f443fa830ae596ee2f0
valerie <valeriemaite@outlook.com>
2022-07-04 12:36:15 -0400

    🟢 helper funcitons

---
+++ kcic-ui/src/utils/formatter.ts
@@ -0,0 +1,14 @@
+const getCurrencyValue = (num) => {
+    return new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+
+    }).format(num)
+};
+
+const getTodaysDate = ()=>{
+ const today = new Date();;
+ return `${String(today.getDate()).padStart(2, '0')}-${String(today.getMonth() + 1).padStart(2, '0')}-${today.getFullYear()}`   
+}
+
+export { getCurrencyValue, getTodaysDate };
